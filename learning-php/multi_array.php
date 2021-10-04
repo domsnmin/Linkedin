54a673b58d76a6b63da53b192a02cb4b5af9edcd
Dom Son <domsnmin@gmail.com>
2021-10-05 01:46:37 +1100

    second commit

---
+++ learning-php/multi_array.php
@@ -0,0 +1,35 @@
+<?php
+    $brothers = array(
+        'Joe' => array(
+            'age' => 34,
+            'job' => 'teacher',
+            'state' => 'PA',
+        ),
+        'Phil' => array(
+            'age' => 33,
+            'job' => 'photographer',
+            'state' => 'NY',
+        ),
+        'Mike' => array(
+            'age' => 32,
+            'job' => 'logistics',
+            'state' => 'NY',
+        ),
+        'Rob' => array(
+            'age' => 29,
+            'job' => 'manager',
+            'state' => 'FL',
+        ),
+    );
+
+    echo '<pre>';
+    print_r( $brothers );
+    echo '</pre>';
+
+    echo '<P>' . $brothers['Mike']['job'] . '</P>';
+
+    // This won't work...!
+    // <p>
+    //     echo $brothers['Rob']['state'];
+    // </p>
+?>
